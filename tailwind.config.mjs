bbec916c5a8b79f86ba6686a4561653fc9472847
Marco <305316+mmbianco78@users.noreply.github.com>
2025-06-19 15:23:01 -0400

    applied new basic style guide and theme and updated config and tailwind to get working stable base

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,25 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-  ],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       colors: {
-        primary: '#FF6B1A',
-        accent: '#D57A18',
-        secondary: '#3F8CFF',
-        highlight: '#AE7FFF',
-        bg: '#111112',
-        surface: '#1E1E1E',
-        'text-primary': '#F2F2F2',
-        'text-secondary': '#A0A0A0',
+        'primary': '#FF6B1A',
+        'accent': '#D57A18',
+        'secondary': '#3F8CFF',
+        'highlight': '#AE7FFF',
+        'bg': '#111112',
+        'surface': '#1E1E1E',
+        'surface-contrast': '#171717',
+        'text': {
+          'primary': '#F2F2F2',
+          'secondary': '#A0A0A0'
+        }
       },
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-        heading: ['Poppins', 'sans-serif'],
+        sans: ['Inter', 'Open Sans', 'sans-serif'],
+        heading: ['Poppins', 'Inter', 'sans-serif'],
       },
+      borderRadius: {
+        'md': '8px',
+      }
     },
   },
   plugins: [],
-};
+}
